99d7052c67de4956111422b1f97ec57914f2c617
Hala Zbib <halazbib22@gmail.com>
2022-02-14 17:37:25 +0200

    added request status to add.php

--- api/add.php
+++ api/add.php
@@ -11,15 +11,12 @@ $bad_request['message'] ='Bad Request';
 
 // get the user data of the person i wanna add 
 // and then add that person to friends and pend the request
-
-    $query = $db->prepare("INSERT INTO friends(user_id,friend_id) VALUES (?,?)"); 
-    $query->bind_param("ss", $user_id,$friend_id);
+    $request = "pending";
+    $query = $db->prepare("INSERT INTO friends(user_id,friend_id,request) VALUES (?,?,?)"); 
+    $query->bind_param("sss", $user_id,$friend_id,$request);
     $query->execute();
 
-    $array_response["status"] = "Added Successfully!";
-
-$json_response = json_encode($array_response);
-echo $json_response;
+    echo json_encode(['request' => `${request}`]);
 
 $query->close();
 $db->close();
