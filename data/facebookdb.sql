5220995155da2ea99eacc9454a46cb54a6d70f4d
Hassan-Zbib <hassan.zbib01@gmail.com>
2022-02-16 16:24:52 +0200

    added postman coll. and db update

--- data/facebookdb.sql
+++ data/facebookdb.sql
@@ -1,32 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Feb 14, 2022 at 04:31 PM
--- Server version: 10.4.11-MariaDB
--- PHP Version: 7.4.3
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `facebookdb`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `blocks`
---
 
 CREATE TABLE `blocks` (
   `id` int(11) NOT NULL,
@@ -35,25 +15,20 @@ CREATE TABLE `blocks` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `friends`
---
+INSERT INTO `blocks` (`id`, `user_id`, `friend_id`, `created_at`) VALUES
+(1, 1, 8, '2022-02-16 15:35:12'),
+(2, 6, 1, '2022-02-16 15:42:39');
 
 CREATE TABLE `friends` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `friend_id` int(11) NOT NULL,
-  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
-  `request` varchar(25) NOT NULL
+  `request` varchar(25) NOT NULL DEFAULT 'pending',
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `likes`
---
+INSERT INTO `friends` (`id`, `user_id`, `friend_id`, `request`, `created_at`) VALUES
+(1, 1, 7, 'accepted', '2022-02-14 17:47:45');
 
 CREATE TABLE `likes` (
   `id` int(11) NOT NULL,
@@ -62,19 +37,9 @@ CREATE TABLE `likes` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `likes`
---
-
 INSERT INTO `likes` (`id`, `user_id`, `status_id`, `created_at`) VALUES
-(11, 3, 5, '2022-02-14 10:29:09'),
-(12, 7, 5, '2022-02-14 13:26:21');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `statuses`
---
+(80, 1, 6, '2022-02-16 01:19:06'),
+(82, 1, 10, '2022-02-16 01:19:09');
 
 CREATE TABLE `statuses` (
   `id` int(11) NOT NULL,
@@ -85,146 +50,85 @@ CREATE TABLE `statuses` (
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `statuses`
---
-
 INSERT INTO `statuses` (`id`, `user_id`, `content`, `likes_count`, `created_at`, `updated_at`) VALUES
-(2, 1, 'tessssssssssssssst', 0, '2022-02-14 06:25:03', '2022-02-14 06:25:21'),
-(5, 1, 'fdgdfhdshsdfgyahedrfyha', 2, '2022-02-14 08:55:30', '2022-02-14 13:26:21'),
-(6, 7, 'fdgdfhdshsdfgyahedrfyha', 0, '2022-02-14 12:13:15', '2022-02-14 12:13:15'),
-(7, 7, 'fdgdfhdshsdfgyahedrfyha', 0, '2022-02-14 12:13:29', '2022-02-14 12:13:29'),
-(8, 7, 'fdgdfhdshsdfgyahedrfyha', 0, '2022-02-14 12:13:30', '2022-02-14 12:13:30'),
-(9, 7, 'fdgdfhdshsdfgyahedrfyha', 0, '2022-02-14 12:13:31', '2022-02-14 12:13:31'),
-(10, 7, 'fdgdfhdshsdfgyahedrfyha', 0, '2022-02-14 13:24:33', '2022-02-14 13:24:33');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
+(6, 7, 'fdgdfhdshsdfgyahedrfyha', -45, '2022-02-14 12:13:15', '2022-02-16 01:19:06'),
+(7, 7, 'fdgdfhdshsdfgyahedrfyha', 3, '2022-02-14 12:13:29', '2022-02-16 01:14:26'),
+(8, 7, 'fdgdfhdshsdfgyahedrfyha', -9, '2022-02-14 12:13:30', '2022-02-16 00:30:14'),
+(9, 7, 'fdgdfhdshsdfgyahedrfyha', 8, '2022-02-14 12:13:31', '2022-02-16 01:48:01'),
+(10, 7, 'fdgdfhdshsdfgyahedrfyha', -8, '2022-02-14 13:24:33', '2022-02-16 01:19:09'),
+(11, 3, 'jklhfvhkzxgffdljfvyhlkjc', -9, '2022-02-14 18:45:38', '2022-02-16 01:03:11'),
+(15, 1, 'ewrtfewqrtfewtrftrdfsgtsrfgsgffsg', 0, '2022-02-15 22:52:44', '2022-02-16 14:46:19'),
+(16, 1, 'dfgdg', 0, '2022-02-16 01:23:09', '2022-02-16 01:23:09');
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `picture` longblob DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Dumping data for table `users`
---
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `picture`, `created_at`, `updated_at`) VALUES
+(1, 'Hassan Zbib', 'hassan.zbib01@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', NULL, '2022-02-12 19:40:44', '2022-02-12 19:40:44'),
+(3, 'Hala Zbib', 'Halazbib22@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', NULL, '2022-02-12 19:42:09', '2022-02-12 19:42:19'),
+(6, 'test', 'HAssan.zbib02@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', NULL, '2022-02-13 03:20:20', '2022-02-13 03:20:20'),
+(7, 'testttt', 'test@test.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', NULL, '2022-02-14 06:41:55', '2022-02-14 06:41:55'),
+(8, 'testttt', 'tes5t@test.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', NULL, '2022-02-15 09:09:58', '2022-02-15 09:09:58');
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(1, 'Hassan Zbib', 'hassan.zbib01@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '2022-02-12 19:40:44', '2022-02-12 19:40:44'),
-(3, 'Hala Zbib', 'Halazbib22@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '2022-02-12 19:42:09', '2022-02-12 19:42:19'),
-(6, 'test', 'HAssan.zbib02@gmail.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '2022-02-13 03:20:20', '2022-02-13 03:20:20'),
-(7, 'testttt', 'test@test.com', 'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3', '2022-02-14 06:41:55', '2022-02-14 06:41:55');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `blocks`
---
 ALTER TABLE `blocks`
   ADD PRIMARY KEY (`id`),
   ADD KEY `blocker_id` (`user_id`),
   ADD KEY `blocked_id` (`friend_id`);
 
---
--- Indexes for table `friends`
---
 ALTER TABLE `friends`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`),
   ADD KEY `friend_id` (`friend_id`);
 
---
--- Indexes for table `likes`
---
 ALTER TABLE `likes`
   ADD PRIMARY KEY (`id`),
   ADD KEY `status_id` (`status_id`),
   ADD KEY `liked_user_id` (`user_id`);
 
---
--- Indexes for table `statuses`
---
 ALTER TABLE `statuses`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `blocks`
---
 ALTER TABLE `blocks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `friends`
---
 ALTER TABLE `friends`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
---
--- AUTO_INCREMENT for table `likes`
---
 ALTER TABLE `likes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=83;
 
---
--- AUTO_INCREMENT for table `statuses`
---
 ALTER TABLE `statuses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `blocks`
---
 ALTER TABLE `blocks`
   ADD CONSTRAINT `blocked_id` FOREIGN KEY (`friend_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `blocker_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Constraints for table `friends`
---
 ALTER TABLE `friends`
-  ADD CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`friend_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `friend_id` FOREIGN KEY (`friend_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Constraints for table `likes`
---
 ALTER TABLE `likes`
   ADD CONSTRAINT `liked_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `status_id` FOREIGN KEY (`status_id`) REFERENCES `statuses` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
---
--- Constraints for table `statuses`
---
 ALTER TABLE `statuses`
   ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
